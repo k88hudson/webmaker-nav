60b3e02ea62c0ed14480fe97f2d382f9da8d5c3b
Kate Hudson <katehudsondesign@gmail.com>
2012-10-12 16:46:10 -0400

    Added some transition stuff

--- main.js
+++ main.js
@@ -2,16 +2,17 @@ define( [ "webmaker-nav/webmaker-nav"], function( Webmaker ) {
 
   // Normally you would pass your actual app 
   var testApp = {
-    username: "testuser"
+    username: "testuser@mozilla.org"
   };
 
+  // Don't actually call view functions here. Just for demonstration
   var webmakerNav = new Webmaker( testApp, {
     container: document.querySelector( "#webmaker" ),
     loginBtnCallback: function() {
-      console.log( "login" );
+      webmakerNav.views.login();
     },
-    logoutBtnCallbak: function() {
-      console.log( "logout" );
+    logoutBtnCallback: function() {
+      webmakerNav.views.logout();
     },
     feedbackCallback: function() {
       console.log( "do feedback stuff" );
